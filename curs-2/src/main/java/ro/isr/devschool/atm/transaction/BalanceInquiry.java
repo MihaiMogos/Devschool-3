162dce14f8504b3abc30ad6d0d3e64550c6da8d6
mihaimogos <mihai.mogos@gmail.com>
2017-03-16 19:56:02 +0200

    curs 2

---
+++ curs-2/src/main/java/ro/isr/devschool/atm/transaction/BalanceInquiry.java
@@ -0,0 +1,24 @@
+package ro.isr.devschool.atm.transaction;
+
+import ro.isr.devschool.atm.BankDatabase;
+import ro.isr.devschool.atm.Screen;
+
+public class BalanceInquiry extends Transaction {
+
+    public BalanceInquiry(int accountNumber, Screen screen, BankDatabase bankDatabase) {
+        super(accountNumber, screen, bankDatabase);
+    }
+
+    @Override
+    public void execute() {
+        BankDatabase bankDatabase = getBankDatabase();
+        Screen screen = getScreen();
+
+        double totalBalance = bankDatabase.getTotalBalance(getAccountNumber());
+
+        screen.displayMessageLine("\nBalance Information:");
+        screen.displayMessage("\n - Total balance:     ");
+        screen.displayDollarAmount(totalBalance);
+        screen.displayMessageLine("");
+    }
+}
