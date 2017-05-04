84549699fa3511d5ce6b01d00c166224f6568fc1
mihaimogos <mihai.mogos@gmail.com>
2017-05-04 19:13:03 +0300

    curs 8 - springboot

---
+++ curs-8/src/main/java/ro/isr/devschool/jpa/Employee.java
@@ -0,0 +1,22 @@
+package ro.isr.devschool.jpa;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Tolerate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by Mihai MOGOS on 04/05/17.
+ */
+@Entity @Getter @Setter @Builder
+public class Employee {
+    @Tolerate public Employee(){}
+
+    @Id private Long id;
+    @Column private String name;
+    @Column private double salary;
+}
---
+++ curs-8/src/main/resources/readme.txt
@@ -0,0 +1,4 @@
+Venkat Subramaniam
+https://www.youtube.com/watch?v=Ee5t_EGjv0A
+Josh Long - Bootifull Microservices
+https://www.youtube.com/watch?v=rqQOSG0DWPY
